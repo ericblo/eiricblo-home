7d4c0b38fb5a317ad73f41a41ec19bddf87a95f7
Eric Blohm <blohm.eric@gmail.com>
2026-04-23 13:23:47 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,24 +8,10 @@ const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: 'ericblo',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  description: 'Data Analytics & Strategy', // Might as well update this too!
   icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
+    icon: '/favicon.png', // This forces one icon for both light/dark modes
+    apple: '/favicon.png',
   },
 }
 
