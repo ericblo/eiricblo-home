049ece3192b41e6c3671ec496a33c5e3dc6b04c0
Eric Blohm <blohm.eric@gmail.com>
2026-04-26 13:35:43 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,3 +9,16 @@ const nextConfig = {
 }
 
 export default nextConfig
+
+async rewrites() {
+  return [
+    {
+      source: '/balticway',
+      destination: 'https://v0-baltic-way-visualization.vercel.app/balticway',
+    },
+    {
+      source: '/balticway/:path*',
+      destination: 'https://v0-baltic-way-visualization.vercel.app/balticway/:path*',
+    },
+  ]
+}
